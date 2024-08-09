f7078d251cd5f47ea445c140f78f93be8500d774
nehagjr <pneha3169@gmail.com>
2024-08-09 18:39:55 +0530

    basic authentication

--- viewSetAPI/project/api/urls.py
+++ viewSetAPI/project/api/urls.py
@@ -3,9 +3,10 @@ from rest_framework import routers
 from .views import UserViewSet
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('', include('api.urls'))
-]
+    path('', include(router.urls))
+    path('api-auth/', include('rest_framework.urls'))
+]   
--- viewSetAPI/project/api/views.py
+++ viewSetAPI/project/api/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import SnippetSerializer
 from .models import Snippet
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    A viewset for viewing and editing user instances.
-    """
-    serializer_class = SnippetSerializer
+    permission_classes = [IsAuthenticated]
     queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
